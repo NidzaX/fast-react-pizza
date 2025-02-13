16c94e77d66a7c71bcb56e7c5b63f78119818408
NidzaX <59400424+NidzaX@users.noreply.github.com>
2025-02-13 16:19:13 +0100

    Writing, Fetching Data and Error Handling

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,22 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "no-unused-vars": "warn", // No blocking for unused variables
+    "react/no-unescaped-entities": "warn", // No blocking for unescaped entities
+    "react/prop-types": "warn", // Change prop-types errors to warnings
   },
-}
+};
